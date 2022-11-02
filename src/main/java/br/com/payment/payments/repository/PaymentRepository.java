3665227255ee5c62286d8e26bdcc59dabb97f7d2
odair-pedroso <odairpedroso90@gmail.com>
2022-11-02 15:01:05 -0300

    create-repository

---
+++ src/main/java/br/com/payment/payments/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package br.com.payment.payments.repository;
+
+import br.com.payment.payments.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository <Payment, Integer>{
+}
